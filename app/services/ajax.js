2f6f72b60ddcdb9335f741221e98715372834780
Ping <ping.teo@mscsea.co>
2016-02-16 11:32:19 +0800

    Installed ember-ajax

---
+++ app/services/ajax.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+import ENV from 'web/config/environment';
+import AjaxService from 'ember-ajax/services/ajax';
+
+export default AjaxService.extend({
+  session: Ember.inject.service(),
+  host: ENV['ember-simple-auth'].authorizerHost,
+  headers: Ember.computed('session.data.authenticated.access_token', {
+    get() {
+      let headers = {};
+      console.log("*** Setting ajax header; ", this.get('session.data.authenticated.access_token'));
+      const authToken = this.get('session.data.authenticated.access_token');
+      if (authToken) {
+        headers['Authorization'] = 'Bearer ' + authToken;
+      }
+      return headers;
+    }
+  })
+
+});
