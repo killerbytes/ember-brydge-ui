eacb1c6a096302cbe8770fb90e5475e43f68cfc8
Ping <ping.teo@mscsea.co>
2016-02-22 16:28:06 +0800

    List notifications

--- app/models/notification.js
+++ app/models/notification.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr('string'),
   content: DS.attr('string'),
+  summary: DS.attr('string'),
   link: DS.attr(),
   actionLink: DS.attr(),
   shortid: DS.attr(),
