93106ac8a48a097af93fb82dd759864792a12e86
Ping <ping.teo@mscsea.co>
2016-02-24 23:38:06 +0800

    Update admin panel component to use didInsertElement to init foundation()

--- app/components/admin-menu.js
+++ app/components/admin-menu.js
@@ -3,15 +3,16 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   session: Ember.inject.service('session'),
 
-  // didInsertElement() {
-  //   "use strict";
-  //   this.$(document).foundation();
-  // },
-
-  didRender() {
+  didInsertElement() {
     "use strict";
     this.$(document).foundation();
   }
+
+  // See https://guides.emberjs.com/v2.3.0/components/the-component-lifecycle/#toc_making-updates-to-the-rendered-dom-with-code-didrender-code
+  // didRender() {
+  //   "use strict";
+  //   this.$(document).foundation();
+  // }
 
 
 });
