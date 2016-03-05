128c823ba860b167b413c8bedfe4efe9d30ce65e
Ping <ping.teo@mscsea.co>
2016-03-06 00:17:02 +0800

    xxx

--- app/models/conversation.js
+++ app/models/conversation.js
@@ -3,5 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   you: DS.belongsTo('user'),
   other: DS.belongsTo('user'),
-  messages: DS.hasMany('message',{ async: true })
+  messages: DS.hasMany('message', { async: true })
 });
