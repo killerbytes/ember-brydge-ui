6e64d61df846be61617795d88782de98b3c30869
Hein Zeya <hein@Heins-MacBook-Pro.local>
2016-03-16 12:18:12 +0800

    added public profile posts

--- app/models/public-profile.js
+++ app/models/public-profile.js
@@ -4,5 +4,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
 	firstName: DS.attr('string'),
 	lastName: DS.attr('string'),
-	location: DS.attr('string')
+	location: DS.attr('string'),
+	userid: DS.attr('string')
 });
--- app/profile/route.js
+++ app/profile/route.js
@@ -1,8 +1,27 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
 	model: function(params) {
-		console.log('public profile', params);
-		return this.store.findRecord('public-profile', params.username);
+    return this.store.findRecord('public-profile', params.username);
+  },
+
+  afterModel: function(model, transaction) {
+  	console.log('<<<<',model.get('firstName'),
+  		model.get('lastName'),
+  		model.get('location'),
+  		model.get('userid'));
+  	
+  	let userid = model.get('userid');
+    let _this = this;
+
+	  return this.store.query('post',{userid: userid }).then(function (trendingPosts) {
+	    _this.set('trendingPosts', trendingPosts);
+	  });
+  },
+
+  setupController: function(controller, model) {
+	  controller.set('model', model)
+	  controller.set('trendingPosts', this.get('trendingPosts'))
 	}
 });
