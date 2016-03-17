60b37fd568e6c40711aab1093e33f36fe4b324b0
Ping <ping.teo@mscsea.co>
2016-03-17 11:28:05 +0800

    WIP: Added job title

--- app/models/profile.js
+++ app/models/profile.js
@@ -4,5 +4,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
 	firstName: DS.attr('string'),
 	lastName: DS.attr('string'),
+	jobTitle: DS.attr(),
 	location: DS.attr('string')
 });
