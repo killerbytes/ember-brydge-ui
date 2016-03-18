e64b8b3c5ba2438b66674c7aa9ef8871acd98573
Ping <ping.teo@mscsea.co>
2016-03-18 17:05:08 +0800

    Minor update to messages to fit new api

--- app/models/message.js
+++ app/models/message.js
@@ -1,8 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  from: DS.belongsTo('user',{ async: true }),
+  from: DS.belongsTo('user'),
   to: DS.belongsTo('user'),
   content: DS.attr('string'),
-  conversation: DS.belongsTo('conversation')
+  createdAt: DS.attr('date')
+  //conversation: DS.belongsTo('conversation')
 });
