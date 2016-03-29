82255cd3828c42df1d3b57ca6bd7b8dfb5043fae
Ping <ping.teo@mscsea.co>
2016-03-30 02:12:18 +0800

    Transit to /me if viewing own profile

--- app/profile/route.js
+++ app/profile/route.js
@@ -1,36 +1,51 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  session: Ember.inject.service('session'),
 
-	model: function(params) {
+  beforeModel(transition) {
+    const myUsername = this.get('session.data.authenticated.username');
+    const paramsUsername = transition.params['profile'].username;
+
+    // console.log("Transit to own profile?", myUsername, paramsUsername, (myUsername === paramsUsername));
+
+    if (myUsername === paramsUsername) {
+      this.transitionTo('me');
+      return;
+    }
+  },
+
+  model: function(params) {
     this.set('username', params.username);
     return this.store.findRecord('public-profile', params.username);
   },
 
   afterModel: function(model, transition) {
 
-		console.log(model);
+    console.log(model);
 
-  	console.log('<<<<',model.get('firstName'),
-  		model.get('lastName'),
-  		model.get('location'),
-  		model.get('userid'),
+    console.log('<<<<', model.get('firstName'),
+      model.get('lastName'),
+      model.get('location'),
+      model.get('userid'),
       this.get('username'));
 
-  	let userid = model.get('userid');
+    let userid = model.get('userid');
     let _this = this;
 
-		if (!userid) return true; 
+    if (!userid) return true;
 
-		return this.store.query('post', {userid: userid }).then(function (trendingPosts) {
-	    _this.set('trendingPosts', trendingPosts);
-	  });
+    return this.store.query('post', {
+      userid: userid
+    }).then(function(trendingPosts) {
+      _this.set('trendingPosts', trendingPosts);
+    });
 
   },
 
   setupController: function(controller, model) {
     model.username = this.get('username');
-	  controller.set('model', model)
-	  controller.set('trendingPosts', this.get('trendingPosts'))
-	}
+    controller.set('model', model)
+    controller.set('trendingPosts', this.get('trendingPosts'))
+  }
 });
