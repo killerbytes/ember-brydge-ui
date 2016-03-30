d42f8cb73e1b2940e9d87cc85c694e0390843d8f
Joel Carlos <joelcarlos@Joels-MacBook-Pro.local>
2016-03-30 10:19:43 +0800

    ui stuff

--- app/components/newsfeed-item/component.js
+++ app/components/newsfeed-item/component.js
@@ -1,5 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	classNames: ['newsfeed-item', 'box']
+	ajax: Ember.inject.service(),
+	classNames: ['newsfeed-item', 'box', 'rounded'],
+	actions: {
+		vote(){
+
+			console.log('newsfeed-item', this.get('ajax').request('/v1/posts/0z931mwmhde/up'));
+
+		}
+	}
 });
