41316533624389ba6042f10e973e4b86b9c38eb5
Hein Zeya <hein@Heins-MacBook-Pro.local>
2016-03-30 09:49:03 +0800

    put back post detail

---
+++ app/routes/post.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+
+  model(params) {
+   console.log("routes/post.js>>>>>>", params);
+    const userid = params.user_id;
+    const newsfeedid = params.newsfeed_id;
+    return this.store.find('newsfeed', newsfeedid);
+  }
+});
