ed29c9194d85a91626b9d85469aa425a0da8327a
Joel Carlos <joelcarlos@Joels-MacBook-Pro.local>
2016-03-31 09:30:33 +0800

    curated bar

--- app/components/newsfeed-item/component.js
+++ app/components/newsfeed-item/component.js
@@ -3,7 +3,13 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 	ajax: Ember.inject.service(),
 	classNames: ['newsfeed-item', 'box', 'rounded'],
+	menuName: Ember.computed('post.id', function(){
+		return "dd-post-menu-" + this.get('post.id');
+	}),
 	actions: {
+		focusInput(){
+			this.$('input').focus()
+		},
 		vote(){
 
 			console.log('newsfeed-item', this.get('ajax').request('/v1/posts/0z931mwmhde/up'));
