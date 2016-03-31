5d4c6a5c8d7c6ee5c3072dc147d3a14f9819101c
Hein Zeya <hein@Heins-MacBook-Pro.local>
2016-03-31 10:53:12 +0800

    added selectize and countries mixin

--- app/components/particular-info/component.js
+++ app/components/particular-info/component.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
+import GetCountryListMixin from 'web/mixins/get-country-list';
 
-export default Ember.Component.extend({
+export default Ember.Component.extend(GetCountryListMixin,{
 	actions: {
 		save: function () {
 			this.get('profile').save().then(()=>{
