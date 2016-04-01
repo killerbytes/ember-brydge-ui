86c3d2b344068e7208039af81c9a67102ca3e42f
Hein Zeya <hein@Heins-MacBook-Pro.local>
2016-04-01 11:25:32 +0800

    added reusable spinning button

---
+++ app/components/spinning-button/component.js
@@ -0,0 +1,88 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+	tagName: 'button',
+
+  // classNames: ['button','large','connect'],
+
+	attributeBindings: [ 'disabled', 'title'],
+
+	isSpinning: false,
+
+	disabled: false,
+
+	lines: 8,
+
+  radius: 5,
+
+  length: 4,
+
+  width: 3,
+
+  corners: 1,
+
+  rotate: 0,
+
+  direction: 1,
+
+  speed: 1,
+
+  trail: 60,
+
+  shadow: false,
+
+  hwaccel: false,
+
+  color: null,
+
+  left: '50%',
+
+  top: '50%',
+
+  timeOut: 1000,
+
+	setPositionToRelative: Ember.on('didInsertElement', function () {
+    this.$().css('position', 'relative');
+  }),
+
+  maintainButtonDimensions: Ember.on('didInsertElement', Ember.observer('isSpinning', function () {
+    if (this.get('isSpinning')) {
+      this.$().css({
+        'width': this.$().outerWidth() +'px',
+        'height': this.$().outerHeight() +'px'
+      });
+    } else {
+      this.$().css({
+        'width': '',
+        'height': ''
+      });
+    }
+  })),
+
+  click() {
+    let _this = this;
+
+    if (!this.get('isSpinning')) {
+      this.set('isSpinning', true);
+      this.set('disabled', true);
+
+      if(this.get('action')) {
+        this.sendAction( 'action', function(){
+          console.log('callback');
+
+          //
+          // add delay after callback
+          //
+          Ember.run.later((function() {
+            _this.set('isSpinning', false);
+            _this.set('disabled', false);
+          }), _this.get('timeOut'));
+        });
+      }
+
+      if (typeof this.get('onclick') === 'function') {
+        this.get('onclick')();
+      }
+    }
+  }
+});
