8fa6ea273561af84d40018e834a408158040f838
Hein Zeya <hein@Heins-MacBook-Pro.local>
2016-04-06 14:13:33 +0800

    wireup user own ask page

--- app/me/ask/route.js
+++ app/me/ask/route.js
@@ -1,7 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	session: Ember.inject.service('session'),
 	model: function() {
-		return this.store.findAll('ask');
+		//return this.store.findAll('ask');
+
+		let ownerid = this.get('session.data.authenticated.user_id');
+		let username = this.get('session.data.authenticated.username');
+		console.log('ownerid,username =>', ownerid,username);
+
+		return Ember.RSVP.hash({
+			username: username,
+      fromQuestions: this.store.query('ask',{from: ownerid}),
+      toQuestions: this.store.query('ask',{to: ownerid})
+    })
 	}
 });
