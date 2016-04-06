6c19e5c8cdbdc1a080a758cc7e2104f5aa2b7a8f
Hein Zeya <hein@Heins-MacBook-Pro.local>
2016-04-07 01:34:56 +0800

    fixed upload avatar and wire up

--- app/components/particular-info/component.js
+++ app/components/particular-info/component.js
@@ -3,15 +3,22 @@ import GetCountryListMixin from 'web/mixins/get-country-list';
 
 export default Ember.Component.extend(GetCountryListMixin,{
 	flashMessages: Ember.inject.service(),
-
+	ajax: Ember.inject.service(),
 	actions: {
 		save: function () {
 			this.get('profile').save().then(()=>{
 				Ember.get(this, 'flashMessages').success('Success!');
 			});
 		},
-		fileLoaded: function(file){
-      console.log('particular info Component=>',file);
+		fileLoaded: function(formData){
+      console.log('particular info Component=>');
+
+      return this.get('ajax').request('/v1/profile/avatar', {
+        method: 'POST',
+        data: formData,
+        contentType: false,
+        processData: false,
+      });
     }
 	}
 });
