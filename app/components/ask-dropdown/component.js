d7ef2919cb9dc4d7d09627abdd547c18c18e9b54
Joel Carlos <joel@brydge.com>
2016-04-07 10:59:15 +0800

    ask dropdown

--- app/components/ask-dropdown/component.js
+++ app/components/ask-dropdown/component.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	classNames: ['wrapper', 'button', 'expanded', 'large', 'text-blue', 'box', 'rounded'],
 	actions: {
 		ask: function(to, from, username,question) {
 			console.log('ask dropdown to/from/username/question', to,from,username,question);
