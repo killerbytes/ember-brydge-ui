fff3cdda52e7501aab4be38eb429df89807167c8
Hein Zeya <hein@Heins-MacBook-Pro.local>
2016-04-08 15:28:48 +0800

    answer uncomment out

--- app/components/answer-form/component.js
+++ app/components/answer-form/component.js
@@ -10,16 +10,16 @@ export default Ember.Component.extend({
 			console.log('answer =>', selectedQuestion.get('answer'))
 
 			
-			// var store = this.store;
-			// var savedCallback = function() {
-			// 	console.log('saved ask');
-			// 	this.$('#answerFormModal').foundation('close');
-			// };
+			var store = this.store;
+			var savedCallback = function() {
+				console.log('saved ask');
+				this.$('#answerFormModal').foundation('close');
+			};
 
-			// store.findRecord('ask', selectedQuestion.get('id')).then(function(ask){
-			// 	ask.set('answer', selectedQuestion.get('answer'));
-			// 	ask.save().then(savedCallback);
-			// });
+			store.findRecord('ask', selectedQuestion.get('id')).then(function(ask){
+				ask.set('answer', selectedQuestion.get('answer'));
+				ask.save().then(savedCallback);
+			});
 		}
 	}
 });
