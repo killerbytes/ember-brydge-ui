941cbfbe9a4f151b47d4b1e6455296b62a7be512
Joel Carlos <joel@brydge.com>
2016-04-15 16:37:41 +0800

    pending request

--- app/requests/route.js
+++ app/requests/route.js
@@ -1,5 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	session: Ember.inject.service('session')
+	session: Ember.inject.service('session'),
+	model: function(){
+		var userid = this.get('session.data.authenticated.user_id');
+		return this.store.query('connection',{to: userid});
+	}
 });
