2f92a84dbbdbf0704f1228804b0a6ec9d1d8398f
Hein Zeya <hein@Heins-MacBook-Pro.local>
2016-04-22 13:06:07 +0800

    edit profile controller cleanup

--- app/me/edit/controller.js
+++ app/me/edit/controller.js
@@ -8,39 +8,7 @@ export default Ember.Controller.extend(
   QueryLocationMixin,
   AvatarMixin,
   SaveProfileMixin,
-  QueryIndustryMixin,
-  {
+  QueryIndustryMixin,{
     queryParams: ['tab'],
-    tab: 'personal',
-    
-    // add work form
-    isNewWork: false,
-    // add education form
-    isNewEducation: false,
-    //
-    // select 2 binding
-    //
-    chosenIndustry: null,
-    chosenOccupTwo: null,
-    chosenOccupOne: null,
-    //
-    // actions
-    //
-    selectedItem: null,
-
-    actions: {
-    	addNewWork: function() {
-    		this.set('isNewWork', true);
-    	},
-    	
-    	addNewEducation: function() {
-    		this.set('isNewEducation', true);
-    	},
-      cancel: function(){
-        console.log('cancel')
-      },
-      selectItem: function(item){
-        this.set('selectedItem', item)
-      }
-    }
+    tab: 'personal'
 });
