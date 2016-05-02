5ac16fcb7e6ed0afebdf02ad852933b8f480b9c7
Joel Carlos <joel@brydge.com>
2016-05-02 13:58:37 +0800

    added Froala WYSIWYG Editor

--- app/components/answer-form/component.js
+++ app/components/answer-form/component.js
@@ -1,23 +1,26 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  froalaEditor: {
+    params: {
+      placeholderText: 'Type answer here...',
+		  pastePlain: true,
+      toolbarButtons: [],
+      toolbarButtonsMD: [],
+      toolbarButtonsSM: [],
+      toolbarButtonsXS: [],
+    },
+  },
 	actions: {
 		answer: function () {
-			let selectedQuestion = this.get('selectedQuestion');
-			
-			console.log('question =>', selectedQuestion.get('content'));
-			console.log('ask id =>', selectedQuestion.get('id'));
-			console.log('answer =>', selectedQuestion.get('answer'))
-
-			
-			var store = this.store;
-			var savedCallback = function() {
-				console.log('saved ask');
-			};
-
-			store.findRecord('ask', selectedQuestion.get('id')).then(function(ask){
-				ask.set('answer', selectedQuestion.get('answer'));
-				ask.save().then(savedCallback);
+			let selectedQuestion = this.get('selectedQuestion');			
+			this.store.findRecord('ask', selectedQuestion.get('id')).then((ask)=>{
+				ask.set('answer', this.$('.froalaEditor').froalaEditor('html.get'));
+				ask.save().then(()=>{
+					this.$('.froalaEditor').froalaEditor('html.set', null);
+					this.$('#answerFormModal').foundation('close');
+					console.log('saved ask');
+				});
 			});
 		}
 	}
--- app/testing/controller.js
+++ app/testing/controller.js
@@ -3,9 +3,19 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   queryParams: ['category'],
   category: 'azid',
+  froalaEditor: {
+    params: {
+      placeholder: 'Enter..',
+      // toolbarButtons: [],
+      // toolbarButtonsMD: [],
+      // toolbarButtonsSM: [],
+      // toolbarButtonsXS: [],
+        // For more params refer: 'https://www.froala.com/wysiwyg-editor/docs/options'
+    },
+  },
   actions: {
   	change: function(){
-  		this.set('category', 'sdfsdf')
+      this.set('output', $('.froalaEditor').froalaEditor('html.get'));
   	}
   }
 });
