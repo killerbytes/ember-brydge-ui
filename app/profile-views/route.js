4a95ecd0423e56e6a39c05ede0ee2bf471b7ddf0
Joel Carlos <joel@brydge.com>
2016-05-09 10:39:44 +0800

    profile-views setting

--- app/profile-views/route.js
+++ app/profile-views/route.js
@@ -1,5 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	session: Ember.inject.service('session')
+	session: Ember.inject.service('session'),
+	settings: Ember.inject.service(),
+	model(){
+		let userid = this.get('session.data.authenticated.user_id');
+		return this.store.findRecord('profile', userid);
+	},
+	setupController(controller, model){
+		this._super(...arguments);
+    controller.set('settingsChanged', function(value) {
+      this.send('settingsChanged', value);
+    });    
+
+	},
+  actions: {
+    settingsChanged(value){
+    	let profile = this.currentModel;
+    	profile.set('settings.'+value, !profile.get('settings.'+value))
+      this.get('settings').update(profile.get('settings'));
+    }
+  }
 });
