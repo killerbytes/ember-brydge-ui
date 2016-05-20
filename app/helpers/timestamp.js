33e2b79322cca7ea6698b0255ce4409fa3373f88
Joel Carlos <joel@brydge.com>
2016-05-20 15:29:13 +0800

    timestamp

---
+++ app/helpers/timestamp.js
@@ -0,0 +1,28 @@
+import Ember from 'ember';
+
+export default Ember.Helper.helper(([date])=>{
+  var today = moment();
+  var date = moment(date);
+  var dd = today.diff(date, 'seconds')
+  switch(true){
+    case dd < 60: // < 60 seconds
+      return 'Just now';
+      break;
+    case dd < 3600: // < 60 minutes
+      return  Math.floor(dd/60) + ' minutes ago';
+      break;
+    case dd < 86400: // < 24 hours
+      return  Math.floor(dd/60/60) + ' hours ago';
+      break;
+    case dd < 172800: // < 48 hours
+      return  'Yesterday at ' + date.format("h:mma");
+      break;
+    default:
+      if(today.year() == date.year()){
+        return  date.format("MMMM D [at] h:mma");          
+      }else{
+        return  date.format("MMMM D, YYYY [at] h:mma");        
+      }
+      break;
+  }
+});
