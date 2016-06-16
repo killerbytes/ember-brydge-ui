d7ad914e4f0cd1b229e36326a1bf26c282014a88
Joel Carlos <joel@brydge.com>
2016-06-16 11:21:12 +0800

    login form adjustments

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-route-mixin';
 
-export default Ember.Route.extend(UnauthenticatedRouteMixin, {});
+export default Ember.Route.extend(UnauthenticatedRouteMixin, {
+  renderTemplate(){
+    this._super();
+    Ember.run.later(()=>{
+      $('.equalizer').foundation();
+    })
+  }
+
+});
