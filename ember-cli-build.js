a59e2d57752af26eda9067e84647d90777895ba8
Joel Carlos <joel@brydge.com>
2016-06-20 10:18:26 +0800

    loading color

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -21,7 +21,7 @@ module.exports = function(defaults) {
   app.import('bower_components/motion-ui/dist/motion-ui.min.css');
   app.import('bower_components/motion-ui/dist/motion-ui.min.js');
   app.import('bower_components/lodash/dist/lodash.js');
-  app.import('vendor/spinner.css');
+  // app.import('vendor/spinner.css');
   // app.import('bower_components/foundation-sites/js/foundation.core.js');
   // app.import('bower_components/foundation-sites/js/foundation.dropdownMenu.js');
 
