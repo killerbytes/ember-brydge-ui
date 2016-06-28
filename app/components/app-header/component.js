9495019367c8dd90a1cd1fac113da19c2f250800
Joel Carlos <joel@brydge.com>
2016-06-28 20:17:46 +0800

    adjustments

---
+++ app/components/app-header/component.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+import NotificationActionsMixin from 'web/mixins/notification-actions';
+
+export default Ember.Component.extend(NotificationActionsMixin, {
+	sessionAccount: Ember.inject.service(),
+	actions: {
+    closeTooltip(e){
+      $('body').find('.tooltip').hide();
+    }		
+	}
+});
