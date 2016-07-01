ef4c78aa912e6d98a7579cee3464851a32f96ad6
Joel Carlos <joel@brydge.com>
2016-07-01 18:53:44 +0800

    industry picker

---
+++ app/components/picker-industry/component.js
@@ -0,0 +1,35 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+	actions: {
+		close(){
+			this.$('.industry-picker').foundation('toggle');
+		},
+    goto(index, item){
+    	switch(index){
+    		case "0":
+		      this.$('#industry-tab-'+ this.get('name')).foundation('selectTab', $('#'+ this.get('name') + '-root'));
+    			break;    		
+    		case "1":
+    			this.set('sector', item);
+
+		      this.$('#industry-tab-'+ this.get('name')).foundation('selectTab', $('#'+ this.get('name') + '-' + item.code));
+    			break;
+    		case "2":
+    			this.set('group', item);
+		      this.$('#industry-tab-'+ this.get('name')).foundation('selectTab', $('#'+ this.get('name') + '-' + item.code));
+    			break;
+    		case "3":
+    			this.set('industry', item)
+		      this.$('#industry-tab-'+ this.get('name')).foundation('selectTab', $('#'+ this.get('name') + '-done'));
+    			break;
+    	}
+      // this.$('#industry-tab-'+ this.get('name')).foundation('selectTab', $('[data-id="'+link+'"]'));
+    },
+    select(item){
+    	this.sendAction('onSelect', item);
+			this.$('.industry-picker').foundation('toggle');
+    }
+
+	}
+});
