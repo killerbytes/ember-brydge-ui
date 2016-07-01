3eb4ecc42a7b06fa4084a44a663237405127a978
Hein Zeya <hein@Heins-MacBook-Pro.local>
2016-07-01 15:53:27 +0800

    change vote serializer

--- app/serializers/vote.js
+++ app/serializers/vote.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 export default DS.JSONAPISerializer.extend({
   
   keyForAttribute: function(attr) {
-    return attr;
+    return Ember.String.underscore(attr);
   }
 
 });
