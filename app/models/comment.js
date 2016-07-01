29239fb2328123c97e120ddf2bafc9a43fdbefeb
Hein Zeya <hein@Heins-MacBook-Pro.local>
2016-07-01 12:47:25 +0800

    fixed comments

--- app/models/comment.js
+++ app/models/comment.js
@@ -4,5 +4,5 @@ export default DS.Model.extend({
   content: DS.attr('string'),
   threadId: DS.attr(),
   user: DS.belongsTo('user'),
-  createdAt: DS.attr()
+  insertedAt: DS.attr()
 });
--- app/serializers/comment.js
+++ app/serializers/comment.js
@@ -24,6 +24,6 @@ export default DS.JSONAPISerializer.extend({
     
  },
   keyForAttribute: function(attr) {
-    return attr;
+    return Ember.String.underscore(attr);
   }
 });
