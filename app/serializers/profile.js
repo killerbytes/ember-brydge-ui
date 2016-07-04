ac5126b364aec526e9d6ff85965a168b15fbca80
Hein Zeya <hein@Heins-MacBook-Pro.local>
2016-07-04 10:38:00 +0800

    reformat profile serializer profile

--- app/serializers/profile.js
+++ app/serializers/profile.js
@@ -6,20 +6,25 @@ export default DS.JSONAPISerializer.extend(DS.EmbeddedRecordsMixin, {
       attr = result.data.attributes || {},
       rel = result.data.relationships || {};
 
-    var payloadData= Object.keys(rel).reduce(function(acc, elem) {
+    // var payloadData= Object.keys(rel).reduce(function(acc, elem) {
      
-      const data = rel[elem].data;
-      if (data) {
-        acc[elem + "_id"] = data.id;
-      }
-      if (data && data.type) {
-        acc[elem + "_type"] = data.type[0].toUpperCase() + data.type.slice(1, -1);
-      }
-      return acc;
+    //   const data = rel[elem].data;
+    //   if (data) {
+    //     acc[elem + "_id"] = data.id;
+    //   }
+    //   if (data && data.type) {
+    //     acc[elem + "_type"] = data.type[0].toUpperCase() + data.type.slice(1, -1);
+    //   }
+    //   return acc;
 
-    }, attr);
-    return payloadData;
-    
+    // }, attr);
+
+    // return payloadData;
+
+    var payloadData = attr;
+    return {
+      profile: payloadData
+    };
  },
 
   keyForAttribute: function(attr) {
