7268a5174758718616d2a77fc2d7bb9ac28ee684
Joel Carlos <joel@brydge.com>
2016-07-07 09:59:32 +0800

    ask/compliments see all logic

--- app/mixins/filtered-questions.js
+++ app/mixins/filtered-questions.js
@@ -21,6 +21,9 @@ export default Ember.Mixin.create({
 	from: Ember.computed('fromQuestions.@each.status', 'fromQuestions.@each.delete', function(){
 		return {
 			pendingSidebar: this.get('fromPendingQuestions').slice(0, this.get('limit')),
+			showLink: Ember.computed('fromPendingQuestions', ()=>{
+				return this.get('fromPendingQuestions').length > this.get('limit');
+			}),
 			pending: this.get('fromPendingQuestions'),
 			answered: this.get('fromAccept'),
 			hiddenSidebar: this.get('fromHidden').slice(0, 2),
@@ -31,6 +34,9 @@ export default Ember.Mixin.create({
 	to: Ember.computed('toQuestions.@each.status', 'toQuestions.@each.delete', function(){
 		return {
 			pendingSidebar: this.get('toPendingQuestions').slice(0, this.get('limit')),
+			showLink: Ember.computed('toPendingQuestions', ()=>{
+				return this.get('toPendingQuestions').length > this.get('limit');
+			}),
 			pending: this.get('toPendingQuestions'),
 			answered: this.get('toAccept'),
 			hiddenSidebar: this.get('toHidden').slice(0, 2),
