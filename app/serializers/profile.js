f498aca3680b5864df62c0a07d43d8cc92a68b02
Hein Zeya <hein@Heins-MacBook-Pro.local>
2016-07-11 22:21:09 +0800

    fixed profile serializer

--- app/serializers/profile.js
+++ app/serializers/profile.js
@@ -6,24 +6,24 @@ export default DS.JSONAPISerializer.extend(DS.EmbeddedRecordsMixin, {
       attr = result.data.attributes || {},
       rel = result.data.relationships || {};
 
-    // var payloadData= Object.keys(rel).reduce(function(acc, elem) {
-     
-    //   const data = rel[elem].data;
-    //   if (data) {
-    //     acc[elem + "_id"] = data.id;
-    //   }
-    //   if (data && data.type) {
-    //     acc[elem + "_type"] = data.type[0].toUpperCase() + data.type.slice(1, -1);
-    //   }
-    //   return acc;
-
-    // }, attr);
-
-    // return payloadData;
-
-    var payloadData = attr;
     return {
-      profile: payloadData
+      profile: {
+        avatar: attr.avatar_url,
+        current_company: attr.current_company,
+        current_title: attr.current_title,
+        custom_title: attr.custom_title,
+        dob: attr.dob,
+        first_name: attr.first_name,
+        industry_one_id: attr.industry_one_id,
+        industry_one_name: attr.industry_one_name,
+        industry_three_id: attr.industry_three_id,
+        industry_three_name: attr.industry_three_name,
+        industry_two_id: attr.industry_two_id,
+        industry_two_name: attr.industry_two_name,
+        last_name: attr.last_name,
+        location: attr.location,
+        snapshot: attr.snapshot
+      }
     };
  },
 
