c6ddaf593c05c03a8afd643b01f3fd797c15ec4f
Joel Carlos <joel@brydge.com>
2016-07-20 17:58:59 +0800

    compliment status accepted

--- app/compliments/route.js
+++ app/compliments/route.js
@@ -6,9 +6,9 @@ export default Ember.Route.extend({
     return Ember.RSVP.hash({
       profile: this.store.findRecord('profile', userid),
       connections: this.store.query('connection',{userid: userid}),
-      toCompliments: this.store.query('compliment',{to: params.userid, userid: userid}),
+      toCompliments: this.store.query('compliment',{to: userid, status: 'accepted'}),
       questions: this.store.query('ask',{ userid: userid, per_page: 1, page:1 }),
-      compliments: this.store.query('compliment',{to: userid, per_page: 1, page: 1})
+      compliments: this.store.query('compliment',{to: userid, status: 'accepted', per_page: 1, page: 1})
     })
   }
 });
