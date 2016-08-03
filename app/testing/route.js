743ccaebc38863399370d61592085b4f3ad5d4e6
Joel Carlos <joel@brydge.com>
2016-08-03 11:51:18 +0800

    work experience current company

--- app/testing/route.js
+++ app/testing/route.js
@@ -1,9 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model(){
-    return this.store.findRecord('profile', '2zd33na16gv')
-  },
+  // model(){
+  //   return this.store.findRecord('profile', '2zd33na16gv')
+  // },
   actions: {
     save(){
       this.set('controller.model.dob', moment({year: 1979, month: 1, date: 15}).format());
