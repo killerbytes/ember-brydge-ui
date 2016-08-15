27315c4fc7b1179a0e9f7bc7a09f03c1fb25963c
Joel Carlos <joel@brydge.com>
2016-08-15 20:53:44 +0800

    comments

--- app/helpers/html-safe-image.js
+++ app/helpers/html-safe-image.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Helper.helper(function([url, preview=true]){
-	console.log(preview)
 	var url = url || (preview ? 'assets/blank-user.jpg' : '');
 
 	return Ember.String.htmlSafe("background-image: url(" + url + ')' );
