a1deb75093e9b74a778a7e3d2a3142209b653dcb
Hein Zeya <hein@Heins-MacBook-Pro.local>
2016-08-19 19:07:36 +0800

    m

--- app/initializers/backend.js
+++ app/initializers/backend.js
@@ -3,8 +3,9 @@ import {Socket} from "phoenix";
 export function initialize(application) {
 	// let a = application.get('session.data.authenticated');
 	// console.log(a)
-  let socket = new Socket("ws://localhost:8000/ws",{params: {token: "fb16401a651e11e69211985aeb89a4c0"}});
-  socket.connect();
+  let socket = new Socket("ws://localhost:8000/ws");
+  socket.connect({"token": "c5075fa0643c11e6b0da985aeb89a4c0"})
+  
   // let chan = socket.chan("rooms:lobby", {});
   // chan.join().receive("ok", chan => {
   //   console.log("Welcome to Phoenix Chat!");
