284ac0a8dd64f6d49b907dee37f4dde1b150ebf6
Joel Carlos <joel@brydge.com>
2016-08-23 13:43:44 +0800

    removed unused packages

--- app/me/ask/hidden/route.js
+++ app/me/ask/hidden/route.js
@@ -1,17 +1,25 @@
 import Ember from 'ember';
-import InfinityRoute from "ember-infinity/mixins/route";
+import BrydgeScroller from 'web/mixins/brydge-scroller';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend(InfinityRoute, AuthenticatedRouteMixin, {
+export default Ember.Route.extend(
+	BrydgeScroller, 
+	AuthenticatedRouteMixin, {
 	session: Ember.inject.service(),
 	model: function() {
 		let userid = this.get('session.data.authenticated.user_id');
-		return this.infinityModel('ask',{
+		return this.brydgeScroller('ask',{
 			userid: userid,
-			perPage: 5,
-			startingPage: 1,
 			status: 'hide',
+			scroller: 'hidden'
 		});
+
+		// return this.infinityModel('ask',{
+		// 	userid: userid,
+		// 	perPage: 5,
+		// 	startingPage: 1,
+		// 	status: 'hide',
+		// });
 
 	}
 
