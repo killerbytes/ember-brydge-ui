1641af5178d72eea72f8419cd560575e57b2aeac
Joel Carlos <joel@brydge.com>
2016-08-26 16:29:30 +0800

    channel filters

--- app/components/picker-channel/component.js
+++ app/components/picker-channel/component.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
 import _ from 'lodash/lodash';
+import SharePostIndustryPicker from 'web/mixins/industry';
 
-export default Ember.Component.extend({
+export default Ember.Component.extend(SharePostIndustryPicker, {
 	ask: Ember.inject.service(),
 	utils: Ember.inject.service(),
 	store: Ember.inject.service(),
@@ -11,31 +12,34 @@ export default Ember.Component.extend({
 		// return this.get('selected') ? this.getIndustryName(this.get('selected')) : this.getIndustryName(0);
 	}),
 	isIndustry: Ember.computed.or('profile.industryOneId', 'profile.industryTwoId', 'profile.industryThreeId'),
-	industries: Ember.computed('profile', function(){
-		return [{
-      id: this.get('profile.industryOneId'),
-      text: this.get('profile.industryOneName')
-    },{
-      id: this.get('profile.industryTwoId'),
-      text: this.get('profile.industryTwoName')
-    },{
-      id: this.get('profile.industryThreeId'),
-      text: this.get('profile.industryThreeName')
-    }];
-	}),
+	// industries: Ember.computed('profile', function(){
+	// 	return [{
+  //     id: this.get('profile.industryOneId'),
+  //     text: this.get('profile.industryOneName')
+  //   },{
+  //     id: this.get('profile.industryTwoId'),
+  //     text: this.get('profile.industryTwoName')
+  //   },{
+  //     id: this.get('profile.industryThreeId'),
+  //     text: this.get('profile.industryThreeName')
+  //   }];
+	// }),
   getCategory(value){
-    var categories = this.get('categories');
+    var categories = this.get('menu');
+		console.log(value, categories);
+
     return _.chain(_.map(categories, 'categories'))
                .flatten()
-               .map((i)=>{ return i.industries; })
+               .map((i)=>{ console.log(i); return i.industries; })
                .flatten()
-               .filter((d)=>{ return d.data.code == value; })
+               .filter((d)=>{ console.log(d); return d.data.code == value; })
                .map((i)=>{ return {code: i.data.code, name: i.data.subIndustry } })
                .first()
                .value();
   },
 
 	getIndustryName: function (id) {
+		console.log(id)
     var col = {};
     col[0] = "My Connections";
     col[this.get('profile.industryOneId')] = this.get('profile.industryOneName');
