f62c172d8f71324c892cc05ff3440e874b8a1210
Joel Carlos <joel@brydge.com>
2016-09-05 10:21:57 +0800

    bg template

---
+++ app/mixins/route-class-names.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+const {
+  Component,
+  computed,
+  getOwner
+} = Ember;
+
+export default Ember.Mixin.create({
+  resetController(controller, isExiting, transition) {
+      if (isExiting) {
+        getOwner(this).lookup('controller:application').set('classNames', '');
+      }
+  },
+  actions: {
+    didTransition(){
+      getOwner(this).lookup('controller:application').set('classNames', `page-${this.get('routeName')}`);
+
+    }
+  }
+});
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,13 +1,22 @@
 import Ember from 'ember';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
+import RouterClassNamesMixins from 'web/mixins/route-class-names';
+
 
 export default Ember.Route.extend(
+  RouterClassNamesMixins,
   ApplicationRouteMixin, {
   session: Ember.inject.service('session'),
   beforeModel() {
     this._super(...arguments);
     if (this.get('session.isAuthenticated')) {
       this.transitionTo('home');
+    }
+  },
+  actions: {
+    didTransition(){
+      // console.log('didTransition index.js')
+      this._super(...arguments);
     }
   }
 });
