e33211a53b398b5dcf5cbd5f9207fdcce8e2a0aa
Joel Carlos <joel@brydge.com>
2016-09-05 11:42:31 +0800

    register page

--- app/routes/register.js
+++ app/routes/register.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
+import RouterClassNamesMixins from 'web/mixins/route-class-names';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(RouterClassNamesMixins, {
 	model: function(params){
 		if(!params.code) return;
 		return this.store.findRecord('invitation', params.code).catch(err=>{
