8f00a26615ee068f3607e880bd5140eb521349ab
Joel Carlos <joel@brydge.com>
2016-09-13 15:38:12 +0800

    settings save message

--- app/components/form-input-button/component.js
+++ app/components/form-input-button/component.js
@@ -2,20 +2,27 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 	text: 'Save',
+	_reset(){
+		if(this.get('isDestroyed') || this.get('isDestroying')) return false;
+
+		this.set('isSuccess', false);
+		this.set('warning', false);
+		this.$('.faded').removeClass('error');
+		Ember.run.later(()=>{
+			this.set('customPlaceHolder', null);
+		},500);
+	},
 	actions: {
 		submit(){
 			this.set('isLoading', true);
 			this.sendAction('submit', this.get('item'), (success=true, placeholder, warning)=>{
 				if(this.get('isDestroyed') || this.get('isDestroying')) return false;
-				if(placeholder) this.set('placeholder', placeholder);
+				if(placeholder) this.set('customPlaceHolder', placeholder);
 				if(warning) this.set('warning', warning);
 				this.set('isLoading', false);
 				this.set('isSuccess', success);
 				Ember.run.later(()=>{
-					if(this.get('isDestroyed') || this.get('isDestroying')) return false;
-					this.set('isSuccess', false);
-					this.set('warning', false);
-					this.$('.faded').removeClass('error')
+					this._reset();
 				}, 5000)
 			});
 		}
