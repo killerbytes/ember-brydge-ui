10d998586e53d5f90f7121c0df01dcfd56ed12a8
Joel Carlos <joel@brydge.com>
2016-09-23 16:19:42 +0800

    compliment id and message id

--- app/components/industry-item/component.js
+++ app/components/industry-item/component.js
@@ -4,6 +4,13 @@ export default Ember.Component.extend({
 	classNames: ['row', 'collapse', 'industry-item', 'mb'],
 	store: Ember.inject.service(),
 	industryPicker: Ember.inject.service(),
+	_setSelected: Ember.observer('industryPicker.industries.length', function(){
+		this.get('industryPicker.industries').forEach(i=>{
+			var industry = this.get('groups').findBy('industryId', i);
+			industry.set('selected', true)
+		})
+		// this.get('industryPicker.industries');
+	}),
 	groups: Ember.computed('item', function(){
 		return this.get('store').query('industry',{category: this.get('item.id')});
 	}),
@@ -15,7 +22,7 @@ export default Ember.Component.extend({
 		})
 	}),
 	_load(item){
-		console.log(item.set('active', true))
+		// console.log(item.set('active', true))
 		this.get('store').queryRecord('industry', {category: item.get('groupId'), id: item.get('industryId')}).then(res=>{
 			this.set('industryPicker.selected', res);
 		})
